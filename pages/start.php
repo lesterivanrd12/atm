6a481cfa869d46b107262e49641d8ad3ba06af6c
Lester Ivan Dalaguit <new.dalaguit12@gmail.com>
2023-02-22 04:07:18 +0800

    Create db connection file master-key component

--- pages/start.php
+++ pages/start.php
@@ -1,5 +1,5 @@
 
     <div class="start-container">
         <h1>Enter 16 digit key</h1>
-        <?php include('forms/master-key-form.php') ?>
+        <?php include('components/master-key.php') ?>
     </div>
