f96064b24910bfa25ab4d0fdb26e956d81d107d5
Lester Ivan Dalaguit <new.dalaguit12@gmail.com>
2023-03-03 23:27:51 +0800

    Setup favicon in all pages

--- pages/start.php
+++ pages/start.php
@@ -6,8 +6,21 @@
     } 
 ?>
 
-    <?php include('../components/header.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../assets/img/code++.png" type="image/x-icon">
+    <title>Automated Teller Machine</title>
+</head>
+<body>
+    
     <div class="start-container">
         <h1>Hello World!</h1>
         <a href="../pages/logout.php">Logout</a>
     </div>
+
+</body>
+</html>
